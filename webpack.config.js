c1d609729731e9fd7f6b6f6dbabfc5d2fcae0c5f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-08-20 20:22:42 -0700

    Add jsx.

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,14 @@ var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: [APP_DIR + '/index.jsx'],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
+  },
+  resolve: {
+    root: path.resolve(APP_DIR),
+    extensions: ['', '.js', '.jsx']
   },
   module : {
     loaders : [
